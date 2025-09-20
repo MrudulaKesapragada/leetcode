2b606432f9dbbfe9bb9455c31fa5f56630067306
MrudulaKesapragada <mrudulakesapragada@gmail.com>
2025-09-20 10:57:59 +0530

    Time: 2 ms (78.85%) | Memory: 46.4 MB (25.58%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int l=0;
+        for(int r=0;r<nums.length;r++){
+            if(nums[r]!=0){
+                int t=nums[r];
+                nums[r]=nums[l];
+                nums[l]=t;
+                l++;
+            }
+        }
+        
+    }
+}
